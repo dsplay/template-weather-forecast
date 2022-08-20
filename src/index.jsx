126cea07e12e7651310192d57e08da67f1ba25aa
Paulo Coutinho <pcmnac@gmail.com>
2022-08-20 19:20:45 +0100

    🔥 remove workers

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-// import registerServiceWorker from './registerServiceWorker';
+
 import 'moment-timezone';
 import 'moment/locale/fr';
 import 'moment/locale/es';
@@ -16,4 +16,3 @@ import './index.sass';
 import './fonts.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
